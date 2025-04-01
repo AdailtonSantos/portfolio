c49ba35ab7e60d7100a202ca7866f6f71414b60a
adailton.santos <adailtonaparecido69@gmail.com>
2025-03-31 21:13:23 -0300

    Feat: Ajusta header component e adiciona role banner para acessibilidade

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,12 +1,17 @@
 <template>
-  <header>
-    <router-link class="logo" to="/"><img src="logo.png" alt="logo-name-adailton-santos"></router-link>
-    <router-link to="/">Início</router-link>
-    <router-link to="/sobre">Sobre Mim</router-link>
-    <router-link to="/projetos">Projetos</router-link>
-    <a href="https://drive.google.com/file/d/1I2x4d1e9cJSbL8eYvnsnbAl19uWh8GQ-/view?usp=sharing" target="_blank">Curriculo</a>
-    <a href="https://www.linkedin.com/in/adailton-aparecido-dos-santos-751bb922b/" target="_blank" class="social">Linkedin</a>
-    <a href="https://github.com/AdailtonSantos" target="_blank" class="social">GitHub</a>
+  <header role="banner">
+    <img src="logo.png" alt="Nome do dono do portifólio, Adailton Santos">
+    <nav role="navigation">
+      <router-link to="/">Início</router-link>
+      <router-link to="/competencias">Competências</router-link>
+      <router-link to="/sobre">Sobre mim</router-link>
+      <router-link to="/projetos">Projetos</router-link>
+      <a href="https://drive.google.com/file/d/1I2x4d1e9cJSbL8eYvnsnbAl19uWh8GQ-/view?usp=sharing"
+        target="_blank">Curriculo</a>
+      <a href="https://www.linkedin.com/in/adailton-aparecido-dos-santos-751bb922b/" target="_blank"
+        class="social">Linkedin</a>
+      <a href="https://github.com/AdailtonSantos" target="_blank" class="social">GitHub</a>
+    </nav>
   </header>
 </template>
 
@@ -17,40 +22,44 @@ export default {
 </script>
 
 <style scoped>
+header {
+  background-color: white;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 20px;
+  gap: 15px;
+}
+
+nav {
+  display: flex;
+  gap: 15px;
+  align-items: center;
+}
+
+header * {
+  color: black;
+  text-decoration: none;
+}
+
+header .social {
+  background-color: #5670E7;
+  padding: 10px 25px;
+  border: 1px solid #5670E7;
+  border-radius: 15px;
+  transition: all .2s;
+  color: white;
+}
+
+header .social:hover {
+  border: 1px solid white;
+  border-radius: 15px;
+  background-color: transparent
+}
+
+@media (max-width: 959px) {
   header {
-    display: flex;
-    align-items: center;
-    padding: 0 150px;
-    padding-top: 40px;
-    gap: 15px;
+    display: none;
   }
-  .logo {
-    margin-right: auto;
-  }
-
-  header * {
-    color: white;
-    text-decoration: none;
-  }
-
-  header .social {
-    background-color: #5670E7;
-    padding: 10px 25px;
-    border: 1px solid #5670E7;
-    border-radius: 15px;
-    transition: all .2s;
-  }
-
-  header .social:hover {
-    border: 1px solid white;
-    border-radius: 15px;
-    background-color: transparent
-  }
-
-  @media (max-width: 959px) {
-    header {
-      display: none;
-    }
-  }
-
+}
 </style>
