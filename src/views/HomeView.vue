d27a0a95eaf28034d5243603c07a24eee9e21531
adailton.santos <adailtonaparecido69@gmail.com>
2025-03-31 21:14:54 -0300

    Feat: Repaginação geral do layout da seção inicial

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,191 +1,81 @@
 <template>
-  <section class="general">
-    <div>
-      <section class="principal">
-        <p>Olá, meu nome é <span>Adailton Santos</span>, tenho 20 anos e sou
-          desenvolvedor <span>Full-Stack</span>.</p>
-        <div class="techs">
-          <img src="logos/logoVue.png" alt="logo vuejs">
-          <img src="logos/logoNode.png" alt="logo nodejs">
-          <img src="logos/logoPhp.png" alt="logo php">
-        </div>
-        <p>Minha stack principal de desenvolvimento é VueJS e NodeJs, mas
-          também possuo conhecimento e experiência em PHP.</p>
-      </section>
-
-      <section class="extra">
-        <p> Competências adicionais </p>
-        <div>
-          <img v-for="competence of competences" :src="competence.image" alt="" :key="competence.tec"
-            class="competence-logo">
-        </div>
-      </section>
-      <div class="info">
-        <p>São Paulo / SP</p>
-        <p>contato.adailtonapsantos@gmail.com</p>
-      </div>
+  <section class="principal">
+    <div class="content">
+      <h1>Olá, meu nome é <span>Adailton Santos</span><br>
+      sou <span>Desenvolvedor Fullstack</span></h1>
+      <p>Possuo dois anos e meio de experiência como desenvolvedor, minha stack de desenvolvimento é VueJS e NodeJs e atualmente estou
+        realizando um curso de C#.
+      </p>
+      <a href="#projetos"><button class="to-projects">Ver projetos</button></a>
+      <a href="https://drive.google.com/file/d/1I2x4d1e9cJSbL8eYvnsnbAl19uWh8GQ-/view" target="_blank"><button class="to-curriculum">Ver currículo</button></a>
     </div>
-  </section>
+    <img src="perfil.jpeg" alt="Foto do dono do portifólio. Um rapaz negro, utilizando tranças nagô e uma camisa social. Ele possui uma aliança no dedo. A foto está sendo tirada na frente de um espelho redondo.">
+  </section>  
 </template>
 <script>
 
 export default {
   name: 'HomeView',
-  data() {
-    return {
-      competences: []
-    }
-  },
-  mounted() {
-    this.getCompetences()
-  },
-  methods: {
-    getCompetences() {
-      fetch('competencias.json').then(res => res.json()).then(data => {
-        this.competences = data
-      })
-    }
-  }
 }
 
 </script>
 
 <style scoped>
-.general {
-  height: 90dvh;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-
-.scroll-gif {
-  margin-right: 300px;
-  color: white;
-  text-align: center;
-}
-
-.scroll-gif img {
-  width: 100px;
-}
 
 .principal {
-  color: white;
-  font-size: 1.2em;
-  font-weight: 300;
-  margin-left: 150px;
-  max-width: 60ch;
-  line-height: 20px;
-}
-
-.principal span {
-  color: #5670E7;
-}
-
-.techs img {
-  margin: 0 5px;
-}
-
-.extra {
-  color: white;
-  margin-left: 150px;
-  margin-top: 50px;
-  text-align: center;
-  max-width: 60ch;
-  line-height: 30px;
-  border: 1px solid white;
-  border-radius: 25px;
-}
-
-.extra div {
   display: flex;
-  flex-wrap: wrap;
-  gap: 14px;
-  padding: 25px;
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
+  height: 80dvh;
+
+  background-color: #EEF2FF;
 }
 
-.info {
-  position: absolute;
-  color: white;
-  bottom: 40px;
-  right: 150px;
+.content {
+  color: black;
+  max-width: 65ch;
 }
 
-.competence-logo {
-  transition: transform .3s ease;
+h1 {
+  font-size: 2em;
+}
+
+span {
+  color: #2463EB;
+}
+
+p {
+  font-size: 1.2em;
+}
+
+button {
+  padding: 10px 25px;
+  border: 1px solid rgba(0, 0, 0, 0.414);
+  margin: 0 5px;
+  border-radius: 5px;
+
   cursor: pointer;
 }
 
-.competence-logo:hover {
-  transform: translateY(-30%);
+button.to-projects {
+  background-color: #2463EB;
 }
 
-@media (max-width: 1598px) {
-  .scroll-gif {
-    margin-right: 5vw;
-  }
-
-  .principal p {
-    font-size: 0.9em;
-  }
-
-  .techs img {
-    margin: 0 5px;
-    width: 80px;
-  }
-
-  .extra div img {
-    width: 100px;
-  }
+button.to-projects {
+  color: white;
 }
 
-@media (max-width: 1234px) {
-  .scroll-gif {
-    display: none;
-  }
-
-  .general {
-    justify-content: center;
-    text-align: center;
-  }
-
-  .general div:nth-child(1) {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-
-  .principal {
-    margin-left: initial
-  }
-
-  .extra {
-    margin-left: initial;
-    margin-top: initial;
-  }
-
-  .info {
-    position: inherit;
-    padding: 15px;
-    text-align: center;
-  }
+button.to-curriculum {
+  background-color: white;
 }
 
-@media (max-width: 857px) {
+button.to-curriculum {
+  font-weight: bold;
+}
 
-  .general {
-    height: initial
-  }
-
-  .general div:nth-child(1) {
-    width: 100%;
-    padding: 15px
-  }
-
-  .extra {
-    border: none;
-  }
+img {
+  border-radius: 50%;
+  width: 400px;
 }
 
 </style>
