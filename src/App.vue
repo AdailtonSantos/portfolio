a01e7fa245862adacc2d86cf3c36f70fab7b2e9b
adailton.santos <adailtonaparecido69@gmail.com>
2025-04-03 13:10:50 -0300

    Feat: Remove vue transitions e simplifica app.vue por ser página única

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,13 @@
 <template>
-    <HeaderComponent />
-    <MobileHeaderComponent />
-    <main role="main">
-      <router-view v-slot="{ Component }">
-        <transition name="fade" mode="out-in">
-          <Component :is="Component" />
-        </transition>
-      </router-view>
-    </main>
+  <main role="main">
+    <router-view />
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HeaderComponent from '@/components/HeaderComponent.vue'
-import MobileHeaderComponent from '@/components/MobileHeaderComponent.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HeaderComponent,
-    MobileHeaderComponent,
-  }
 }
 
 </script>
@@ -32,6 +19,8 @@ export default {
   font-weight: 00;
   font-style: normal;
   font-variation-settings: "wdth" 100;
+
+  scroll-behavior: smooth;
 }
 
 html,
@@ -40,15 +29,5 @@ body {
   height: 100%;
   margin: 0;
   padding: 0;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity .2s ease-out;
 }
 </style>
